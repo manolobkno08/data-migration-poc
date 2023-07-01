126ac7d689f2d38d5f4fcfc4f2c7a9ca9c14e581
Manuel Gomez <manologm10@hotmail.com>
2023-06-30 19:43:52 -0500

    docker-compose configuration and postgres environment are up

--- src/app/main.py
+++ src/app/main.py
@@ -1,1 +1,20 @@
+# [Standard Library]
+import os
 
+# [3rd Party]
+import uvicorn
+from dotenv import load_dotenv
+from fastapi import FastAPI
+
+load_dotenv('.env')
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host=os.getenv("SERVER"), port=os.getenv("PORT"))
