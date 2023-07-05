cacb0a729141ccf17b0f0747d318b1dc126307f5
Manuel Gomez <manologm10@hotmail.com>
2023-07-05 01:54:25 -0500

    historical data ingestion script finished

--- src/scripts/migrate_historical_data.py
+++ src/scripts/migrate_historical_data.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python3
 # [Standard Library]
 import os
+import shutil
 import sys
 from pathlib import Path
 
 # [3rd Party]
-import pandas as pd
 import numpy as np
+import pandas as pd
+import psycopg2
 from dotenv import load_dotenv
+from sqlalchemy import create_engine
 
 # Add the parent directory to the path so we can import the app modules
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from app.db import DataBaseConnection
+# sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+# from app.db import DataBaseConnection
 
 
 WORKING_DIRECTORY = Path(__file__).resolve().parent.parent
@@ -20,64 +23,66 @@ dotenv_path = os.path.join(WORKING_DIRECTORY, '.env')
 load_dotenv(dotenv_path)
 
 
-def parse_csv_files_and_save(csv_to_migrate):
+def load_to_db(df, table_name):
+    conn_string = f"postgresql+psycopg2://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('POSTGRES_DB')}"
+    db = create_engine(conn_string)
+    conn = db.connect()
 
-    conn = DataBaseConnection(
-        host=os.getenv('DB_HOST'),
-        port=os.getenv('DB_PORT'),
-        database=os.getenv('POSTGRES_DB'),
-        user=os.getenv('POSTGRES_USER'),
-        password=os.getenv('POSTGRES_PASSWORD')
-    )
+    try:
+        df.to_sql(table_name, con=conn,
+                  if_exists='replace', index=False)
+        print(
+            f"====> data migration for '{table_name}' ran successfully\n    File moved to processed zone\n")
+        conn.close()
+        return 0
+    except Exception as e:
+        print(f"====> Error: {e}\n")
+        conn.close()
+        return 1
 
-    for file in os.listdir(csv_to_migrate):
+
+def parse_csv_files_and_save(csv_to_migrate, destiny_path):
+
+    files = os.listdir(csv_to_migrate)
+    # Add "hired_employees.csv" at the beginning of the list to create main table as needed
+    if "hired_employees.csv" in files:
+        files.remove("hired_employees.csv")
+        files.insert(0, "hired_employees.csv")
+
+    for file in files:
         file_path = os.path.join(csv_to_migrate, file)
+        destiny_file_path = os.path.join(destiny_path, file)
 
         if file.split('.')[1] == 'csv':
-
             if file.split('.')[0] == 'hired_employees':
                 df = pd.read_csv(file_path, names=[
                                  'id', 'name', 'datetime', 'department_id', 'job_id'])
-                print(df)
-                print(df.dtypes)
+                res = load_to_db(df, file.split('.')[0])
+                if res == 0:
+                    shutil.move(file_path, destiny_file_path)
 
-                print("\n")
+            elif file.split('.')[0] == 'departments':
+                df = pd.read_csv(file_path, names=['id', 'department'])
+                res = load_to_db(df, file.split('.')[0])
+                if res == 0:
+                    shutil.move(file_path, destiny_file_path)
 
-                #df = df.astype({"department_id":'Int64', "job_id":'Int64'})
-                # columns_to_convert = ['department_id', 'job_id']
-                # for col in columns_to_convert:
-                #     df[col] = pd.to_numeric(df[col], errors='coerce').astype('Int64')
-                # df = df.replace(np.NAType(), float('nan'))
+            elif file.split('.')[0] == 'jobs':
+                df = pd.read_csv(file_path, names=['id', 'job'])
+                res = load_to_db(df, file.split('.')[0])
+                if res == 0:
+                    shutil.move(file_path, destiny_file_path)
 
-                for col in df.columns:
-                    if df[col].dtype == np.float64:
-                        df[col] = df[col].astype('int')
-
-                print(df)
-                print(df.dtypes)
-
-                #conn.historical_data_ingestion(df, file.split('.')[0])
-
-            # if file.split('.')[0] == 'departments':
-            #     df = pd.read_csv(file_path, names=['id', 'department'])
-            #     conn.historical_data_ingestion(df, file.split('.')[0])
-
-            # elif file.split('.')[0] == 'jobs':
-            #     df = pd.read_csv(file_path, names=['id', 'job'])
-            #     conn.historical_data_ingestion(df, file.split('.')[0])
-
-            # elif file.split('.')[0] == 'hired_employees':
-            #     df = pd.read_csv(file_path, names=[
-            #                      'id', 'name', 'datetime', 'department_id', 'job_id'])
-            #     conn.historical_data_ingestion(df, file.split('.')[0])
         else:
             # Send the output into the log file
             print(f"The following file is not a valid csv: {file}")
             continue
 
-        #print(df.head())
+        # print(df.head())
 
 
 if __name__ == '__main__':
     csv_to_migrate = os.path.join(WORKING_DIRECTORY, 'landing_zone')
-    parse_csv_files_and_save(csv_to_migrate)
+    destiny_path = os.path.join(WORKING_DIRECTORY, 'processed')
+
+    parse_csv_files_and_save(csv_to_migrate, destiny_path)
